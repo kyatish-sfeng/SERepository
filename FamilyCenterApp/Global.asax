926a1be63102713151ead2e82d429c4476ae8b88
ykneni <y2kneni@gmail.com>
2015-09-14 23:09:55 -0400

    Initial Check in

---
+++ FamilyCenterApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FamilyCenterApp.MvcApplication" Language="C#" %>
---
+++ FamilyCenterApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FamilyCenterApp.Startup))]
+namespace FamilyCenterApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
